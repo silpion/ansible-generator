9bec3b09e9af044b9dd70599e6668d7bd8446d69
Mark Kusch <mark.kusch@silpion.com>
2016-07-08 08:26:40 +0200

    Add spec configuration to use predictable locales
    Signed-off-by: Mark Kusch <mark.kusch@silpion.com>

--- files/role/spec/spec_helper.rb
+++ files/role/spec/spec_helper.rb
@@ -6,4 +6,6 @@ c = Vagrant.new
 set :backend, :ssh
 set :host, c.ssh_host
 set :ssh_options, :user => c.ssh_user, :port => c.ssh_port, :keys => c.ssh_keys
-set :disable_sudo, true
+
+set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+set :path, '/usr/local/sbin:/usr/sbin:/sbin:$PATH'
